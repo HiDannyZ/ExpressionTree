9e36172aec94ade0f9df6f0bd902ec43e764f8b8
andy051 <huangandy19970827@gmail.com>
2019-04-10 14:19:39 -0400

    Danny is trash

--- p2.cpp
+++ p2.cpp
@@ -4,8 +4,9 @@
 #include <sstream>
 using namespace std;
 
+/*
 struct Node{
-	int data;
+	char data;
 	Node* next;
 };
 
@@ -64,8 +65,90 @@ class LinkedListStack
 		Node* topOfStack;
 		int size;
 };
+*/
 
+struct Node {
+	char value;
+	Node* right;
+	Node* left;
+};
 
+class DataStructure {
+public:
+
+	virtual void build(*char posfix) {
+
+	} = 0
+
+	virtual int eval() {
+
+	} = 0
+};
+
+class ExpressionTree: public DataStructure{
+
+public:
+	void build(char* posfix){
+		
+	}
+
+};
+
+class LinkedListStack
+{
+	//Constructor
+	LinkedListStack() {
+		topOfStack = nullptr;
+		size = 0;
+	}
+	//Deconstructor
+	~LinkedListStack() {
+		delete topOfStack;
+	}
+
+public:
+	//Push to the top of stack
+	void push(char Value) {
+
+		//Creates new Node and pushes it
+		Node* tempNode = new Node;
+
+		tempNode->data = Value;
+		tempNode->next = topOfStack;
+
+		//New Top of Stack is this now
+		topOfStack = tempNode;
+
+		//Increment size of Stack
+		size++;
+
+	}
+	Node* pop() {
+
+		//Checks that we don't pop on empty stack
+		if (topOfStack != NULL) {
+
+			Node* tempNode = topOfStack;
+
+			//Since Cpp Doesn't have auto garbage collecter, we have to delete it ourselves.
+			delete topOfStack;
+
+			topOfStack = tempNode->next;
+
+			//Decrease Size of Stack
+			size--;
+
+			return tempNode;
+		}
+		else {
+			printf("ERROR: TRYING TO POP EMPTY STACK\n");
+		}
+
+	}
+private:
+	Node* topOfStack;
+	int size;
+};
 
 int main() {
 	string input;
