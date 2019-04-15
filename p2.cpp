19fca1c7c63809b8c4fb9a089e526879149952f9
andy051 <huangandy19970827@gmail.com>
2019-04-15 16:47:44 -0400

    UHHh sh*t ton of bugs with the newly added function that i just dont feel like looking at

--- p2.cpp
+++ p2.cpp
@@ -78,7 +78,7 @@ class LinkedListStack
 		int size;
 
 		//Allows This Class to be used in Expression Tree
-		friend class ExpressionTree;
+		friend class PostOrder_ExpressionTree;
 	};
 
 
@@ -86,14 +86,16 @@ class LinkedListStack
 class Interface{
 
 	public:
-		virtual TNode* build(string posfixList) = 0;
+		virtual TNode* build(string List) = 0;
 		virtual int eval(TNode* tree) = 0;
+		virtual void printExpression(TNode* tree) = 0;
+
 };
 
-class ExpressionTree : public Interface{
+class PostOrder_ExpressionTree : public Interface{
 
 	public:
-		TNode* build(string posfixList){
+		TNode* build(string List){
 			//cout << "This is the Given Input: " << posfixLisst << endl;
 
 			//Stack
@@ -104,7 +106,7 @@ class ExpressionTree : public Interface{
 			// Can I delete This Once I'm done with it then? Though, online says that Delete should only be used with New and char *, not just char. Thanks!
 			char CharCopyOfStr[posfixList.length()];
 			
-			strcpy(CharCopyOfStr, posfixList.c_str());
+			strcpy(CharCopyOfStr, List.c_str());
 
 			char* SplitedCharacter;
 
@@ -125,9 +127,6 @@ class ExpressionTree : public Interface{
 					rootNode->right = rightChildNode;
 					rootNode->left = leftChildNode;
 
-					cout << "From If, Pushed this root value in Stack: " << rootNode->value << endl;
-					cout << "The left and right is: " << rootNode->left->value << " " << rootNode->right->value << endl;
-
 					TheStack.push(rootNode);
 					
 
@@ -137,7 +136,6 @@ class ExpressionTree : public Interface{
 
 					tempNode->value = *SplitedCharacter;
 
-					cout << "From Else Pushed this value in Stack: " << tempNode->value <<endl;
 					TheStack.push(tempNode);
 					
 				}
@@ -155,72 +153,168 @@ class ExpressionTree : public Interface{
 			if (Tree == NULL){
 				return 0;
 			}
-			cout << "BEFORE NULL" << endl;
 
 			if (((Tree->left) == NULL) && ((Tree->right) == NULL)) {
-				cout << "TREE-> VALUE IS: " << (int)(Tree->value) << endl;
 
 				return ((int) Tree->value - ((int)48));
 			}
 
-			cout << "after Null" << endl;
-
-			cout << "Before leftVAl and next value is: " << Tree->left->value << endl;
 			int leftVal = eval(Tree->left);
 
 			int rightVal = eval(Tree->right);
 
 			// cout << Tree->value << " ";
 			
-			cout << "Left Val is : " << leftVal << " " << rightVal << endl;
 			if ((Tree->value) == '+'){
-				cout << "Left + Right: " << leftVal + rightVal << endl;
 				return leftVal + rightVal;
 			}
 			
 			if (Tree->value == '-'){
-				cout << "Left - Right: " << leftVal - rightVal << endl;
 				return leftVal - rightVal;
 			}
 			
 			if (Tree->value == '*'){
-				cout << "Left * Right: " << leftVal * rightVal << endl;
 				return leftVal * rightVal;
 			}
 
 			if (Tree->value == '/'){
-				cout << "Left / Right: " << leftVal / rightVal << endl;
 				return leftVal / rightVal;
 			}
-			
 
 		}
+
+		void printExpression(TNode* Tree){
+
+			if(Tree == NULL){
+				return;
+			}
+
+			printExpression(Tree->left);
+
+			cout << Tree->value << " ";
+
+			printExpression(Tree->right);
+
+		}
+
+		string toPostfix(string infix){
+
+			LinkedListStack operatorStack;
+			std::string postfix;
+
+			char* SplitedCharacter;
+
+			SplitedCharacter = strtok(CharCopyOfStr, " ");
+
+			while(SplitedCharacter!= NULL){
+
+				if((*SplitedCharacter == '+') || (*SplitedCharacter == '-') || (*SplitedCharacter == '*') || (*SplitedCharacter == '/')){
+
+					if(operatorStack.topOfStack == NULL){
+
+						TNode* opNode = new TNode;
+						opNode->value = *SplitedCharacter;
+						operatorStack.push(*opNode);
+
+					}
+
+					if(precedenceCheck(*SplitedCharacter) > precedenceCheck(operatorStack.topOfStack->value)){
+						TNode* opNode = new TNode;
+						opNode->value = *SplitedCharacter;
+						operatorStack.push(*opNode);
+					}
+
+					else if( precedenceCheck(*SplitedCharacter) < precedenceCheck(operatorStack.topOfStack->value)){
+						postfix.append(operatorStack.pop()->value);
+						
+						if(operatorStack.topOfStack == NULL){
+
+							TNode* opNode = new TNode;
+							opNode->value = *SplitedCharacter;
+							operatorStack.push(*opNode);
+
+						}
+
+						if(precedenceCheck(*SplitedCharacter) > precedenceCheck(operatorStack.topOfStack->value)){
+							TNode* opNode = new TNode;
+							opNode->value = *SplitedCharacter;
+							operatorStack.push(*opNode);
+						}
+
+						else if( precedenceCheck(*SplitedCharacter) < precedenceCheck(operatorStack.topOfStack->value)){
+							postfix.append(operatorStack.pop()->value);
+							
+						}
+
+						//case when they're equal
+						else{
+							postfix.append(operatorStack.pop()->value);
+							TNode* opNode = new TNode;
+							opNode->value = *SplitedCharacter;
+							operatorStack.push(*opNode);
+						}
+					}
+
+					//case when they're equal
+					else{
+						postfix.append(operatorStack.pop()->value);
+						TNode* opNode = new TNode;
+						opNode->value = *SplitedCharacter;
+						operatorStack.push(*opNode);
+					}
+
+
+
+				}
+
+				else{
+					postfix.append(*SplitedCharacter)
+				}
+		
+			}
+		}
+
+		int precedenceCheck(char* op){
+
+			if(*op == '+' || *op == '-'){
+				return 1;
+			}
+			if(*op == '*' || *op == '/'){
+				return 2;
+			}
+		}
 };
+
+
 
 int main(int argc, char* argv[]) {
 	string input;
 	int type = atoi(argv[1]);
 	cout<<type << endl;
 	//while(cin)
+
 	while(getline(cin, input))
 	{
 		// Here you would be reading the file line by line
 		//getline(cin, input);
 		
 		if(input.length() == 0){
-			cout << "We were given an empty Line" << endl;
 			break;
 		}
 		cout<<"The input is: "<< input << endl;
 
-		ExpressionTree Tree;
+		PostOrder_ExpressionTree Tree;
 
 		Interface *Base = &Tree;
 
 		TNode* Mytree= Base->build(input);
 
+		cout<< "The inorder traversal is: ";
+		Base->printExpression(Mytree);
+		cout << endl;
+
 		int value = Base->eval(Mytree);
-		cout << endl<< value << endl;
+		cout << "The output of the expression is: "<< value << endl << endl;
 
 		/*
 		Here evaluate the current line and printout the result
